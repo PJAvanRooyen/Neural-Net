6e5ca830deaeab7145408ef18765494f1626d172
PJ van Rooyen <pj@umantec.net>
2022-01-29 15:40:47 +0200

    NN-7 FEAT: create neural net controller widget

--- Project/UI/Application/NeuralNetworkRunWidgetView.cpp
+++ Project/UI/Application/NeuralNetworkRunWidgetView.cpp
@@ -1,10 +1,27 @@
 #include "NeuralNetworkRunWidgetView.h"
+#include "./ui_NeuralNetworkRunWidget.h"
 
 namespace UI {
 namespace Application {
 
 NeuralNetworkRunWidgetView::NeuralNetworkRunWidgetView(QWidget *parent)
-    : QWidget(parent) {}
+    : QWidget(parent), ui(new Ui::NeuralNetworkRunWidget) {
+  ui->setupUi(this);
+}
 
+NeuralNetworkRunWidgetView::~NeuralNetworkRunWidgetView() { delete ui; };
 } // namespace Application
 } // namespace UI
+
+void UI::Application::NeuralNetworkRunWidgetView::
+    on_mLearningIterationButton_released() {
+  // TODO:
+  // -move neural network manager to application
+  // -connect this to neural network manager
+  // -store test data so that a test iteration can independently select a
+  // datapoint to run.
+  // -let connection send event to run a test iteration
+}
+
+void UI::Application::NeuralNetworkRunWidgetView::
+    on_mTestIterationButton_released() {}
--- Project/UI/Application/NeuralNetworkRunWidgetView.h
+++ Project/UI/Application/NeuralNetworkRunWidgetView.h
@@ -3,12 +3,28 @@
 
 #include <QWidget>
 
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class NeuralNetworkRunWidget;
+}
+QT_END_NAMESPACE
+
 namespace UI {
 namespace Application {
 
 class NeuralNetworkRunWidgetView : public QWidget {
 public:
   NeuralNetworkRunWidgetView(QWidget *parent = nullptr);
+
+  ~NeuralNetworkRunWidgetView();
+
+private slots:
+  void on_mLearningIterationButton_released();
+
+  void on_mTestIterationButton_released();
+
+private:
+  Ui::NeuralNetworkRunWidget *ui;
 };
 
 } // namespace Application
